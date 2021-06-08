7ef59f500ea26ba97998b4fbe480dd505ca29195
LeoRoma <xiajtn@gmail.com>
2021-06-08 14:55:56 +0100

    Navigation bar set

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ const port = 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        meta:{
-            data:{
-                title: 'Floema',
-                description: 'Metadata description.'
-            }
-        }
-    });
+app.get('/about', (req, res) => {
+    res.render('pages/about');
+})
+
+app.get('/home', (req, res) => {
+    res.render('pages/home');
+})
+
+app.get('/detail/:id', (req, res) => {
+    res.render('pages/detail');
+})
+
+app.get('/collections', (req, res) => {
+    res.render('pages/collections');
 })
 
 app.listen(port, () => {
