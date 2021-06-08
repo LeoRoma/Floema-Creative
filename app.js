620f59fa3f7b297b5e2782b8770cb65d2536aebb
LeoRoma <xiajtn@gmail.com>
2021-06-08 12:01:07 +0100

    Added views folder with index.html and index.pug files

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const espress = require('express');
+const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('');
 })
 
 app.listen(port, () => {
