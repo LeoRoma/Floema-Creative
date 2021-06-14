a63cf27a0c6dfc71a0ffa04ad7f87d6c4bc2b67c
LeoRoma <xiajtn@gmail.com>
2021-06-14 12:11:29 +0100

    Rendered about and meta

--- app.js
+++ app.js
@@ -8,30 +8,30 @@ const port = 3000;
 const Prismic = require('@prismicio/client');
 const PrismicDOM = require('prismic-dom');
 
-const initApi = (req) => {
-    return Prismic.getApi(process.end.PRISMIC_ENDPOINT, {
+const initApi = req => {
+    return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
         accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-        req: req
+        req
     });
 }
 
-const handleLinkResolver = (doc) => {
-  
+const handleLinkResolver = doc => {
+
     // Define the url depending on the document type
     // if (doc.type === 'page') {
     //   return '/page/' + doc.uid;
     // } else if (doc.type === 'blog_post') {
     //   return '/blog/' + doc.uid;
     // }
-    
+
     // Default to homepage
     return '/';
 }
 
 app.use((req, res, next) => {
     res.locals.ctx = {
-      endpoint: process.end.PRISMIC_ENDPOINT,
-      linkResolver: handleLinkResolver
+        endpoint: process.env.PRISMIC_ENDPOINT,
+        linkResolver: handleLinkResolver
     };
     // add PrismicDOM in locals to access them in templates.
     res.locals.PrismicDOM = PrismicDOM;
@@ -42,7 +42,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/about', (req, res) => {
-    res.render('pages/about');
+    initApi(req).then(api => {
+        api.query([
+            Prismic.Predicates.any('document.type', ['about', 'meta']),
+            // Prismic.Predicates.at('document.type', 'about')
+        ]).then(response => {
+            const { results } = response;
+            const [about, meta] = results;
+
+            res.render('pages/about', {
+                about,
+                meta
+            });
+        });
+    });
 })
 
 app.get('/collection', (req, res) => {
