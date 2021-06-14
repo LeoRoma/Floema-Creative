eef93a11c9587f4914bd47839d19ecb7ea2c62f9
LeoRoma <xiajtn@gmail.com>
2021-06-14 11:26:51 +0100

    Added linkResolver and middleware

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 require('dotenv').config();
 
-
-
 const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+
+const Prismic = require('@prismicio/client');
+const PrismicDOM = require('prismic-dom');
+
+const handleLinkResolver = (doc) => {
+  
+    // Define the url depending on the document type
+    // if (doc.type === 'page') {
+    //   return '/page/' + doc.uid;
+    // } else if (doc.type === 'blog_post') {
+    //   return '/blog/' + doc.uid;
+    // }
+    
+    // Default to homepage
+    return '/';
+}
+
+app.use((req, res, next) => {
+    res.locals.ctx = {
+      endpoint: process.end.PRISMIC_ENDPOINT,
+      linkResolver: handleLinkResolver
+    };
+    // add PrismicDOM in locals to access them in templates.
+    res.locals.PrismicDOM = PrismicDOM;
+    next();
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
