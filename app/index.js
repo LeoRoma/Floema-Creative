8f956d85784038838f999fad2b465a4d66ffa5bc
LeoRoma <xiajtn@gmail.com>
2021-07-13 11:11:53 +0100

    Imported other classes in Index.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,20 @@
+import About from './pages/About';
+import Collections from './pages/Collections';
+import Detail from './pages/Collections';
+import Home from './pages/Home';
+
 class App{
   constructor(){
     console.log('app');
+  }
+
+  createPages () {
+    this.pages = {
+      about: new About(),
+      collections: new Collections(),
+      detail: new Detail(),
+      home: new Home()
+    }
   }
 }
 
