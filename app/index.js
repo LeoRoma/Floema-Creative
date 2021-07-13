31c9f370b344c462eb1d589b21e07ce3d590a1db
LeoRoma <xiajtn@gmail.com>
2021-07-13 15:04:55 +0100

    Created Preloader.js and Component.js

--- app/index.js
+++ app/index.js
@@ -53,6 +53,8 @@ class App {
       this.page = this.pages[this.template];
       this.page.create();
       this.page.show();
+      
+      this.addLinkListeners();
     }else{
       console.log("Error!");
     }
