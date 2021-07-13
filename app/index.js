e805d7e24774dea676e1c471cfabb98223b1d3fa
LeoRoma <xiajtn@gmail.com>
2021-07-13 11:20:47 +0100

    Added index.js in Pages folder

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
-import About from './pages/About';
-import Collections from './pages/Collections';
-import Detail from './pages/Collections';
-import Home from './pages/Home';
+import About from './pages/About/index.js';
+import Collections from './pages/Collections/index';
+import Detail from './pages/Collections/index';
+import Home from './pages/Home/index';
 
 class App{
   constructor(){
